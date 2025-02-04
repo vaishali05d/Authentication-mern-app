ebed52d10a9b1bd2495e107b3bc977462a532e4e
vaishali05d <sharma.vaishali.0512@gmail.com>
2025-02-04 21:36:12 +0530

    updated

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -27,7 +27,7 @@ function SignUp() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/signup`;
+            const url = `https://authentication-mern-app-api.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
